e0c3f547cc42c5212cd7d27b12f71012ef76db5e
JoseLuzardo <85593298+JoseLuzardo4@users.noreply.github.com>
2025-01-25 13:08:00 -0500

    Sonidos arreglados

--- Assets/Scripts/PhoneController.cs
+++ Assets/Scripts/PhoneController.cs
@@ -13,6 +13,7 @@ public class PhoneController : MonoBehaviour, IInteractable
     public AudioSource ambienceSound;
     public AudioSource menuMusic;
     
+    
     // Start is called before the first frame update
     void Start()
     {
